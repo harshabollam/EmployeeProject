402e01f531a1e183c82d413e507eb23b3691d41f
Harsha Vardhan <harshavardhanbollam2001@gmail.com>
2026-04-02 16:35:09 +0530

    primary key

--- Models/EmpDto.cs
+++ Models/EmpDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
 {
     public class EmpDto
     {
+        [Key]
         public int EmpId { get; set; }
         public string EmpName { get; set; }
         public string EmpDomain { get; set; }
